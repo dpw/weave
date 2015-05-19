e8f77b38ec4c582985d718ab0cecd32c528660ed
David Wragg <david@wragg.org>
2015-05-19 12:18:37 +0100

    Add a comment to Router's Start method

--- router/router.go
+++ router/router.go
@@ -84,6 +84,9 @@ func NewRouter(config Config, name PeerName, nickName string) *Router {
 	return router
 }
 
+// Start listening for packets from containers, TCP connections and
+// packets forwarded over UDP.  This is separate from NewRouter so
+// that gossipers can register before we start forming connections.
 func (router *Router) Start() {
 	// we need two pcap handles since they aren't thread-safe
 	var pio PacketSourceSink
