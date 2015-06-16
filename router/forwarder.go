aa4f56a5b00b3cf0817d2caa75ae327383a2e001
David Wragg <david@wragg.org>
2015-06-16 15:58:39 +0100

    Remove the LogFrame when fragmenting
    Subsequent commits make this LogFrame call a little tricky to retain,
    and it's not clear that it is really needed.

--- router/forwarder.go
+++ router/forwarder.go
@@ -110,7 +110,6 @@ func (conn *LocalConnection) Forward(df bool, frame *ForwardedFrame, dec *Ethern
 		forwarderDF.Forward(frame)
 		return nil
 	}
-	conn.Router.LogFrame("Fragmenting", frame.frame, dec)
 	// We can't trust the stack to fragment, we have IP, and we
 	// have a frame that's too big for the MTU, so we have to
 	// fragment it ourself.
