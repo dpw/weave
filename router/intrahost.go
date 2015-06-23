7fbb888cc6cec6b2c016e28a8c891cf03233e871
David Wragg <david@wragg.org>
2015-06-23 16:05:56 +0100

    Introduce IntraHost

---
+++ router/intrahost.go
@@ -0,0 +1,29 @@
+package router
+
+// Interface to intra-host packet handling
+type IntraHost interface {
+	// Inject a packet to be delivered locally
+	InjectPacket([]byte) error
+
+	// Start consuming packets from the bridge
+	ConsumePackets(IntraHostConsumer) error
+}
+
+// A function that accepts locally captured packets.  The ethernet
+// decoder is specific to this thread, and will already have been used
+// to to decode the packet data.
+type IntraHostConsumer func([]byte, *EthernetDecoder)
+
+type NullIntraHost struct{}
+
+func (NullIntraHost) InjectPacket([]byte) error {
+	return nil
+}
+
+func (NullIntraHost) ConsumePackets(IntraHostConsumer) error {
+	return nil
+}
+
+func (NullIntraHost) String() string {
+	return "<no intra-host networking>"
+}
