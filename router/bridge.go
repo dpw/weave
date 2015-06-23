dc91e5aa65817fd17b6e16f4439be2a53592b2f7
David Wragg <david@wragg.org>
2015-06-23 16:05:56 +0100

    Introduce Bridge

---
+++ router/bridge.go
@@ -0,0 +1,29 @@
+package router
+
+// Interface to packet handling on the local virtual bridge
+type Bridge interface {
+	// Inject a packet to be delivered locally
+	InjectPacket([]byte) error
+
+	// Start consuming packets from the bridge
+	ConsumePackets(BridgeConsumer) error
+}
+
+// A function that accepts locally captured packets.  The ethernet
+// decoder is specific to this thread, and will already have been used
+// to to decode the packet data.
+type BridgeConsumer func([]byte, *EthernetDecoder)
+
+type NullBridge struct{}
+
+func (NullBridge) InjectPacket([]byte) error {
+	return nil
+}
+
+func (NullBridge) ConsumePackets(BridgeConsumer) error {
+	return nil
+}
+
+func (NullBridge) String() string {
+	return "<no bridge networking>"
+}
